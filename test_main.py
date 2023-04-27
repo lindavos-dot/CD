4a13783a207c0ba27721d2fd472c5eb2fefbff4f
Linda Vos <lindavos123@hotmail.com>
2023-04-27 15:49:10 +0200

    20230427

--- test_main.py
+++ test_main.py
@@ -6,3 +6,8 @@ def test_index():
     
 def test_winc():
     assert main.winc() == 'Whoohooo, laatste opdracht van winc!'
+
+# Aangepast nadat Github Actions/run-tests.yml groen is voor: run-tests, Deploy, Build
+
+def test_hoera():
+    assert main.hoera() == 'Hoera, als je dit ziet dan werkt het!'
